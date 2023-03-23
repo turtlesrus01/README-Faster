99582a66f87ffef58385b9b84cdf3db28d07ec11
Green Camper <stefan.palacios@gmail.com>
2023-03-22 23:18:05 -0500

    Fixed: functions are formatted correctly with pseudocoding

--- index.js
+++ index.js
@@ -4,17 +4,21 @@ const inquirer = require('inquirer');
 
 //Inquirer function to ask for user input
 inquirer
-//User entry prompt
+  //User entry prompt
   .prompt([{}])
-//User entry response handler
+  //User entry response handler
   .then(response => {
+    console.log(response);
 
-
-//README template as variable
-
-//Create README file in root
-  fs.writeFile('README.md',readMeTxt, => {
+    //README template as variable
+    const readMeTxt = ``
+  //Create README file in root
+  fs.writeFile('README.md',readMeTxt, (err) => {
+    if (err) {
+      console.error(err);
+    }
   //File written successfully
+  console.log('Successly created README!')
   });
 
 });
