4faec83d12ff3dd004367a885cb92a2bea4c2131
Green Camper <stefan.palacios@gmail.com>
2023-03-25 16:29:55 -0500

    Added template literal expressions for more prompts.

---
+++ README-Faster.md
@@ -0,0 +1,23 @@
+# Yes
+
+## Subtitle 
+Yes please
+## Description
+
+## Usage 
+
+## Here is an image of the finished site:
+
+![photo of website]()
+
+The deployed website can be reached at this link: 
+
+## Support
+Reach out to me if you have any questions or if you have questions at:
+
+[GitHub discussions]()
+or
+[My GitHub page]()
+
+## License
+[MIT]()
--- index.js
+++ index.js
@@ -10,6 +10,46 @@ inquirer
         type: 'input',
         message: 'What is the title of your repository?',
         name: 'title',
+      },
+      {
+        type: 'input',
+        message: 'One sentence of about your application?',
+        name: 'subtitle',
+      },
+      {
+        type: 'input',
+        message: 'Please write a description of your application.',
+        name: 'description',
+      },
+      {
+        type: 'input',
+        message: 'Please describe how your application can be used.',
+        name: 'usage',
+      },
+      {
+        type: 'input',
+        message: 'Please link an image of your application.',
+        name: 'image',
+      },
+      {
+        type: 'input',
+        message: 'Please describe how your application can be used.',
+        name: 'usage',
+      },
+      {
+        type: 'input',
+        message: 'Please describe how your application can be used.',
+        name: 'usage',
+      },
+      {
+        type: 'input',
+        message: 'Please describe how your application can be used.',
+        name: 'usage',
+      },
+      {
+        type: 'input',
+        message: 'Please describe how your application can be used.',
+        name: 'usage',
       },])
   //User entry response handler
   .then(response => {
@@ -18,29 +58,29 @@ inquirer
     //README template as variable
     const readMeTxt = `# ${response.title}
 
-    ## Subtitle 
+## Subtitle 
+${response.subtitle}
+## Description
+${response.description}
+## Usage 
+${response.usage}
+## Here is an image of the finished site:
 
-    ## Description
+![photo of website]()
 
-    ## Usage 
-   
-    ## Here is an image of the finished site:
-    
-    ![photo of website]()
-    
-    The deployed website can be reached at this link: 
-    
-    ## Support
-    Reach out to me if you have any questions or if you have questions at:
-    
-    [GitHub discussions]()
-    or
-    [My GitHub page]()
-    
-    ## License
-    [MIT]()`
+The deployed website can be reached at this link: 
+
+## Support
+Reach out to me if you have any questions or if you have questions at:
+
+[GitHub discussions]()
+or
+[My GitHub page]()
+
+## License
+[MIT]()`;
   //Create README file in root
-  fs.writeFile('README.md',readMeTxt, (err) => {
+  fs.writeFile('README-Faster.md',readMeTxt, (err) => {
     if (err) {
       console.error(err);
     }
