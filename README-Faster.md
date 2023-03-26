a62beb16f0bf1ea15958eeae71408e69a98e6f16
Green Camper <stefan.palacios@gmail.com>
2023-03-25 21:57:42 -0500

    Feature: Created new function to place licensing logos and links around the README

--- README-Faster.md
+++ README-Faster.md
@@ -1,8 +1,6 @@
-# 1
-
+# 1 [![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)
 ## Description {#Description}
 1
-
 ## Table of Contents 
 [Description](#Description)
 [Installation](#Installation)
@@ -11,22 +9,16 @@
 [Contributing Guidelines](#Contributing)
 [Tests](#Tests)
 [Questions](#Questions)
-
 ## Installation {#Installation}
 1
-
 ## Usage {#Usage}
 1
-
 ## License {#License}
-This application is registered under the MIT License license.
-
+[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0) This application is registered under the Apache License 2.0.
 ## Contributing Guidelines {#Contributing}
 1
-
 ## Test instructions
 1
-
 ## Questions {#Questions}
 Reach out to me if you have any questions at:
 
--- index.js
+++ index.js
@@ -8,7 +8,7 @@ inquirer
   .prompt([
       {
         type: 'input',
-        message: 'What is the title of your repository?',
+        message: 'What is the title of your application?',
         name: 'title',
       },
       {
@@ -23,14 +23,15 @@ inquirer
       },
       {
         type: 'input',
-        message: 'Please describe how your application can be used.',
+        message: 'Please describe the usage of your application.',
         name: 'usage',
       },
       {
         type: 'rawlist',
         message: 'What license have you chosen for your application?',
         name: 'license',
-        choices: ['GNU General Public License v3.0',
+        choices: ['Apache License 2.0',
+        'GNU General Public License v3.0',
         'MIT License',
         'BSD 2-Clause "Simplified" License',
         'BSD 3-Clause "New" or "Revised" License',
@@ -67,12 +68,10 @@ inquirer
   .then(response => {
     console.log(response);
 
-    //README template as variable
-    const readMeTxt = `# ${response.title}
-
+    //README template literal string as variable
+    const readMeTxt = `# ${response.title} ${licenseImg()}
 ## Description {#Description}
 ${response.description}
-
 ## Table of Contents 
 [Description](#Description)
 [Installation](#Installation)
@@ -81,22 +80,16 @@ ${response.description}
 [Contributing Guidelines](#Contributing)
 [Tests](#Tests)
 [Questions](#Questions)
-
 ## Installation {#Installation}
 ${response.installation}
-
 ## Usage {#Usage}
 ${response.usage}
-
 ## License {#License}
-This application is registered under the ${response.license}.
-
+${licenseImg()} This application is registered under the ${response.license}.
 ## Contributing Guidelines {#Contributing}
 ${response.contribution}
-
 ## Test instructions
 ${response.tests}
-
 ## Questions {#Questions}
 Reach out to me if you have any questions at:
 
@@ -105,6 +98,37 @@ or
 [My email](${response.email})
 
 `;
+  //function to determine and place license images
+  function licenseImg() {
+    switch (response.license) {
+      case 'Apache License 2.0':
+        return ('[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)');
+      case 'MIT License':
+        return ('[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)');
+      case 'BSD 2-Clause "Simplified" License':
+        return ('[![License](https://img.shields.io/badge/License-BSD_2--Clause-orange.svg)](https://opensource.org/licenses/BSD-2-Clause)');
+      case 'BSD 3-Clause "New" or "Revised" License':
+        return ('[![License](https://img.shields.io/badge/License-BSD_3--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)');
+      case 'Boost Software License 1.0':
+        return ('[![License](https://img.shields.io/badge/License-Boost_1.0-lightblue.svg)](https://www.boost.org/LICENSE_1_0.txt)');
+      case 'Creative Commons Zero v1.0 Universal':
+        return ('[![License: CC0-1.0](https://licensebuttons.net/l/zero/1.0/80x15.png)](http://creativecommons.org/publicdomain/zero/1.0/)');
+      case 'Eclipse Public License 2.0':
+        return ('[![License](https://img.shields.io/badge/License-EPL_1.0-red.svg)](https://opensource.org/licenses/EPL-1.0)');
+      case 'GNU General Public License v3.0':
+        return ('[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)');
+      case 'GNU Affero General Public License v3.0':
+        return ('[![License: AGPL v3](https://img.shields.io/badge/License-AGPL_v3-blue.svg)](https://www.gnu.org/licenses/agpl-3.0)');
+      case 'GNU General Public License v2.0':
+        return ('[![License: GPL v2](https://img.shields.io/badge/License-GPL_v2-blue.svg)](https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html)');
+      case 'GNU Lesser General Public License v2.1':
+        return ('[![License: LGPL v3](https://img.shields.io/badge/License-LGPL_v3-blue.svg)](https://www.gnu.org/licenses/lgpl-3.0)');
+      case 'Mozilla Public License 2.0':
+        return ('[![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)');
+      case 'The Unlicense':
+        return ('[![License: Unlicense](https://img.shields.io/badge/license-Unlicense-blue.svg)](http://unlicense.org/)');
+    }
+  };
   //Create README file in root
   fs.writeFile('README-Faster.md',readMeTxt, (err) => {
     if (err) {
